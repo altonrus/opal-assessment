4e6d2e4ad338c14dfefaa7ab4ad72cc2e3011c79
altonrus <altonrus@gmail.com>
2022-05-10 10:54:28 -0400

    first commit

---
+++ 2_scripts/01_preprocess_opal_dat.R
@@ -0,0 +1,12 @@
+source("./2_scripts/00_helper_functions.R")
+
+### Opal app data -------------
+x <- dummy("A")
+
+### |-- first step ---------
+
+
+### |-- second step -------
+
+### Cancer registry data ---------
+
---
+++ 2_scripts/03_propensity_score_cohort_creation.R
@@ -0,0 +1,10 @@
+source("./2_scripts/00_helper_functions.R")
+
+#Look at visit data to identify visits that are part of the same episode
+
+#Create a table of each patient's first episode
+
+# Link to Opal data to ID date of Opal account creation, if Opal user
+
+#Classify episodes as 
+# 1. O
